f2343c77e2b0737d64d94fab6b3e68c9cf7cf03d
Michael Cramm <mike@petrofeed.com>
2015-08-13 16:23:53 -0600

    Add Movies, ability to combine relations

--- scenarios/basic_sql.rb
+++ scenarios/basic_sql.rb
@@ -33,6 +33,53 @@ class BasicSql < Scenario
     class CreateUser < ROM::Commands::Create[:sql]
       relation :users
       register_as :create
+    end
+
+    class Movies < ROM::Relation[:sql]
+      register_as :movies
+      dataset :movies
+
+      def find(id)
+        where(id: id).one
+      end
+
+      def by_title(name)
+        where(name: regexp(name))
+      end
+
+      def by_director(director)
+        where(director_id: director[:id])
+      end
+
+      def for_users(users)
+        where(director_id: users.map { |user| user[:id] })
+      end
+
+      private
+
+      def regexp(raw)
+        Regexp.new(Regexp.escape(raw), Regexp::IGNORECASE)
+      end
+    end
+
+    class UserWithMovies < ROM::Mapper
+      relation :users
+      register_as :user_with_movies
+
+      reject_keys true
+
+      attribute :id
+      attribute :name
+      attribute :email
+
+      combine :movies, on: { id: :director_id } do
+        attribute :title
+      end
+    end
+
+    class CreateMovie < ROM::Commands::Create[:sql]
+      relation :movies
+      register_as :create
       result :one
     end
 
@@ -41,7 +88,7 @@ class BasicSql < Scenario
   end
 
   def seed
-    100.times do
+    20.times do
       user_attrs = {
         name: name = FFaker::Name.name,
         email: FFaker::Internet.email(name),
@@ -49,7 +96,16 @@ class BasicSql < Scenario
         birthday: FFaker::Time.date
       }
 
-      rom.command(:users).create.call(user_attrs)
+      user = rom.command(:users).create.call(user_attrs).first
+
+      5.times do
+        movie_attrs = {
+          title: FFaker::Movie.title,
+          director_id: user[:id]
+        }
+
+       rom.command(:movies).create.call(movie_attrs)
+      end
     end
   end
 
@@ -62,12 +118,19 @@ class BasicSql < Scenario
         column :city, String
         column :birthday, DateTime
       end
+
+      db.create_table? :movies do
+        primary_key :id
+        column :title, String
+        column :director_id, Integer
+      end
     end
   end
 
   def drop_tables
     ROM::SQL.gateway.connection.tap do |db|
       db.drop_table(:users)
+      db.drop_table(:movies)
     end
   end
 
